3b935b2a4e6cfe1b77f826030e1dbfbcdee3feda
Samsideen hydara <70798388+samsideen1234@users.noreply.github.com>
2021-03-28 07:38:19 +1400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+import sys
+import logging
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/skills")
+def skills():
+    return render_template("skills.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/photos")
+def photos():
+    return render_template("photos.html")
+
+
+if __name__ == "__main__":
+    
+
+    app.run(debug=True)
+ 
+
+    
+
+
+
+
+
+
+
+ 
+
